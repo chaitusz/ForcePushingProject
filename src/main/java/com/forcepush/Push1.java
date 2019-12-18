2ea333c7e8ee33cdfa21fad9d00adfe82381e6c3
otheruser <somewhere@nowhere.com>
2019-12-18 23:42:56 +0530

    altered Push1.java class file from workspace 2 by otheruser

--- src/main/java/com/forcepush/Push1.java
+++ src/main/java/com/forcepush/Push1.java
@@ -4,7 +4,9 @@ public class Push1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("This is going to be a push1");
+		System.out.println("This is going to be a push1 edit by otheruser");
+		
+		System.out.println("This line is added from workspace2 by otheruser");
 	}
 
 }
